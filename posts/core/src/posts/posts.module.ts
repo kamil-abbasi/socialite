fa29f6551c4aa89b2d4c6a338bb7d9ced20b0b8d
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-06-07 15:15:46 +0200

    exported service

--- posts/core/src/posts/posts.module.ts
+++ posts/core/src/posts/posts.module.ts
@@ -7,5 +7,6 @@ import { PostsRepository } from './posts.repository';
 @Module({
   controllers: [PostsController],
   providers: [PostsService, PostsRepository],
+  exports: [PostsService],
 })
 export class PostsModule { }
