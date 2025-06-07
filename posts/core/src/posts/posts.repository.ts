da6e6d9362b2faecc11c318a56203ef483d7b3b2
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-06-07 14:49:02 +0200

    posts module setup

---
+++ posts/core/src/posts/posts.repository.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PostsRepository {
+  async find() { }
+
+  async findOne() { }
+
+  async create() { }
+
+  async update() { }
+
+  async remove() { }
+}
---
+++ posts/core/src/posts/posts.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PostsService {
+  async find() { }
+
+  async findOne() { }
+
+  async create() { }
+
+  async update() { }
+
+  async remove() { }
+}
