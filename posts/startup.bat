fe39e7791276e786579b0f21f9e5041a70300c15
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-06-07 14:50:05 +0200

    added commands that import sql tables

--- posts/startup.bat
+++ posts/startup.bat
@@ -1,1 +1,3 @@
 docker compose --profile gateway up -d --build
+docker compose exec posts npx drizzle-kit push --force
+docker compose restart posts
--- posts/startup.sh
+++ posts/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 docker compose --profile gateway up -d --build
+docker compose exec posts npx drizzle-kit push --force
+docker compose restart posts
