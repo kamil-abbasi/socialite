2d1ff8f0414e239fbd75ac7297f13d24f0014ae2
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-06-07 14:47:22 +0200

    imported modules

--- posts/core/src/app.module.ts
+++ posts/core/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
+import { PostsModule } from './posts/posts.module';
+import { DatabaseModule } from './database/database.module';
+import { CommonModule } from './common/common.module';
+import { loadConfig } from './config';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    PostsModule,
+    DatabaseModule,
+    CommonModule,
+    ConfigModule.forRoot({
+      load: [loadConfig],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [AppService],
 })
 export class AppModule { }
