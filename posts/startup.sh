dcbe4c9494fe6199607eab52bd971108f376e283
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-06-07 15:25:46 +0200

    passing config file path when pushing drizzle schemas

--- posts/startup.sh
+++ posts/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker compose --profile gateway up -d --build
-docker compose exec posts npx drizzle-kit push --force
+docker compose exec posts npx drizzle-kit push --force --config=/app/src/database/drizzle.config.ts
 docker compose restart posts
