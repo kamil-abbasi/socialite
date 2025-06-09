848989346822b65e290c64f70962658d7b0ae5d7
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-06-10 00:39:30 +0200

    added types, returning dummy post in create method

--- posts/core/src/posts/posts.repository.ts
+++ posts/core/src/posts/posts.repository.ts
@@ -1,14 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
+import { Post } from './entities';
+import { CreatePostDto, UpdatePostDto } from './dto';
+
+const dummyPost: Post = {
+  id: 'some-id',
+  slug: 'hello-world',
+  title: 'Hello, World!',
+  content: 'Lorem ipsum',
+  tags: ['test'],
+  createdAt: 'iso-timestamp',
+  updatedAt: 'iso-timestamp',
+  userId: 'some-user',
+};
+
 @Injectable()
 export class PostsRepository {
-  async find() { }
+  async find(): Promise<Post[]> {
+    return Promise.resolve([]);
+  }
 
-  async findOne() { }
+  async findOne(): Promise<Post | null> {
+    return Promise.resolve(null);
+  }
 
-  async create() { }
+  async create(createDto: CreatePostDto): Promise<Post> {
+    return Promise.resolve(dummyPost);
+  }
 
-  async update() { }
+  async update(updateDto: UpdatePostDto): Promise<void> {
+    return Promise.resolve();
+  }
 
-  async remove() { }
+  async remove(): Promise<void> {
+    return Promise.resolve();
+  }
 }
