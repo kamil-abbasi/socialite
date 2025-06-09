504d3cdb8494df08f3122e73732ae6f863fb690d
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-06-10 00:38:45 +0200

    updated methods will call to post repository methods

--- posts/core/src/posts/posts.service.ts
+++ posts/core/src/posts/posts.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+import { Post } from './entities';
+import { CreatePostDto, UpdatePostDto } from './dto';
+import { PostsRepository } from './posts.repository';
+
 @Injectable()
 export class PostsService {
-  async find() { }
+  constructor(private readonly postsRepository: PostsRepository) { }
 
-  async findOne() { }
+  async find(): Promise<Post[]> {
+    return this.postsRepository.find();
+  }
 
-  async create() { }
+  async findOne(): Promise<Post | null> {
+    return this.postsRepository.findOne();
+  }
 
-  async update() { }
+  async create(createDto: CreatePostDto): Promise<Post> {
+    return this.postsRepository.create(createDto);
+  }
 
-  async remove() { }
+  async update(updateDto: UpdatePostDto): Promise<void> {
+    return this.postsRepository.update(updateDto);
+  }
+
+  async remove(): Promise<void> {
+    return this.postsRepository.remove();
+  }
 }
